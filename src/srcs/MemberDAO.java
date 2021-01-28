ff28feaafa3280070c4291befc9112aeb4962116
HyeonYongLee <HyeonYongLee.dev@gmail.com>
2021-01-28 20:35:58 +0900

    주석 추가

--- src/srcs/MemberDAO.java
+++ src/srcs/MemberDAO.java
@@ -14,46 +14,58 @@ import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 public class MemberDAO {
-	private Connection conn;
-	private PreparedStatement psmt;
-	private DataSource dataFactory;
+	private Connection conn; //connection 인터페이스: sql 문장을 실행하기 전에 필요, statement 객체 생성 위해 필요 
+	private PreparedStatement psmt; //DB와 연동하게 하는 인터페이스(쿼리 전송), SQL을 미리 컴파일해서 재사용 한다, 특정 sql 문장을 정의 및 실행 시킬 수 있음
+	//각각의 인수에 대해 위치 홀더를 사용해서 sql 문장을 정의한다 위치 홀더란('?')로 표현되며, sql 문장이 실행되기 전 실제 값으로 대체됨
+	private DataSource dataFactory; //커넥션 풀(웹 앱과 동시에 연동할 db와의 연결을 미리 설정해 놓는것)	
 	
-	public MemberDAO() {
+	
+	
+	public MemberDAO() { //memberDAO 객체 생성
 		
 		try {
 			Context ctx=new InitialContext();
+			//new InitialContext()는 웹 어플리케이션이 처음으로 배치시 설정된다 즉 자원 저장통 생성
 			Context envContext=(Context) ctx.lookup("java:/comp/env");
+			//모든 설정된 엔트리와 자원은 JNDI namespace의 java:comp/env 부분에 놓인다
+			//java:/comp/env: JNDI 서비스에 접근하기 위한 기본 이름이다
+			//lookup 메서드를 이용해서 저장된 자원을 찾으러 감 즉 등록된 naming서비스(context.xml)에서 자원을 찾고자 할 때 사용하는 메서드
 			dataFactory=(DataSource) envContext.lookup("jdbc/oracle");
+			//톰캣 context.xml에 설정한 name값인 jdbc/oracle을 이용해 톰캣이 미리 연결한 datasource를 받아온다
 		} catch (NamingException e) {
 			e.printStackTrace();
 		}
 		
 	}
 	
-	public List<MemberVO> listMembers() {
-		List<MemberVO> list=new ArrayList<MemberVO>();
+	public List<MemberVO> listMembers() { //vo클래스에 저장된 멤버 필드를 가져와서 저장하는 리스트(listMembers)
+		List<MemberVO> list=new ArrayList<MemberVO>(); //리스트 생성
 		
 		try {
-			conn=dataFactory.getConnection();
-			String query="select * from t_member";
-			System.out.println("prepareStatement: "+query);
-			psmt=conn.prepareStatement(query);
-			ResultSet rs=psmt.executeQuery();
+			conn=dataFactory.getConnection();//datasource를 이용해서 db에 연결한다
+			String query="select * from t_member"; //db연결 후 query문 실행
+			System.out.println("prepareStatement: "+query); //console창에 select문 돌렸다고 표시
+			psmt=conn.prepareStatement(query);//connection인터페이스에서 제공하는 preparestatement 메서드에 sql문을 전달해서 preparestatement 객체 생성
+			ResultSet rs=psmt.executeQuery();//preparestatment 인터페이스에서 제공하는 executeQuery()메서드를 호출해  미리 설정한 sql문 실행(select문 실행시 사용 ->ResultSet 객체 반환)
+			//resultset이란 statement 또는 prestatement 객체로 select문을 사용해서 받아온 레코드 값을 테이블의 형태로 갖게 되는 객체
 			
-			while(rs.next()) {
-				String id=rs.getString("id");
-				String pwd=rs.getString("pwd");
-				String name=rs.getString("name");
-				String email=rs.getString("email");
-				Date joinDate=rs.getDate("joinDate");
-				MemberVO vo=new MemberVO();
-				vo.setId(id);
+			while(rs.next()) { //rs객체에 저장된 값을 반복해서 저장(next()메서드:다음 레코드 행으로 이동)
+				String id=rs.getString("id"); //rs에 저장된 String형 id라는 데이터를 가져와서 변수 id에 저장
+				String pwd=rs.getString("pwd");//rs에 저장된 String형 pwd라는 데이터를 가져와서 변수 pwd에 저장
+				String name=rs.getString("name");//rs에 저장된 String형 name라는 데이터를 가져와서 변수 name에 저장
+				String email=rs.getString("email");//rs에 저장된 String형 email라는 데이터를 가져와서 변수 email에 저장
+				Date joinDate=rs.getDate("joinDate");//rs에 저장된 String형 joinDate라는 데이터를 가져와서 변수 joinDate에 저장
+				
+				MemberVO vo=new MemberVO(); //각 칼럼 값을 다시 memberVO 객체의 속성에 설정한다
+				vo.setId(id); //id값을 vo의 ID로 다시 설정
 				vo.setPwd(pwd);
 				vo.setName(name);
 				vo.setEmail(email);
 				vo.setJoinDate(joinDate);
-				list.add(vo);
+				list.add(vo); //설정된 MemberVO객체를 다시 Arraylist에 저장한다
 			}
+			
+			//resource를 줄이기 위해 close 메서드 이용해서 생성했던 객체들을 닫아준다(jdbc 이용시)
 			rs.close();
 			psmt.close();
 			conn.close();
@@ -62,28 +74,29 @@ public class MemberDAO {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return list;
+		return list; //조회한 레코드의 개수만큼  MemberVO객체를 저장한 ArrayList를 반환
 	}
 
 	public void addMember(MemberVO memberVO) {
 		try {
-			conn=dataFactory.getConnection();
-			String id=memberVO.getId();
-			String pwd=memberVO.getPwd();
+			conn=dataFactory.getConnection(); //Datasource를 이용해 데이터베이스와 연결한다
+			String id=memberVO.getId(); // 테이블에 저장된 회원 정보를 받아온다(select문 실행시 vo에 새로 저장된)
+			String pwd=memberVO.getPwd(); 
 			String name=memberVO.getName();
 			String email=memberVO.getEmail();
 			
-			String query="insert into t_member";
-			query+="(id,pwd,name,email)";
-			query+="values(?,?,?,?)";
-			System.out.println("prepareStatement: "+query);
-			psmt=conn.prepareStatement(query);
-			psmt.setString(1, id);
+			String query="insert into t_member"; //insert문 문자열로 생성
+			query+="(id,pwd,name,email)"; //테이블에 저장된 컬럼명
+			query+="values(?,?,?,?)"; //query에 들어갈 값
+			System.out.println("prepareStatement: "+query); 
+			psmt=conn.prepareStatement(query); //connection인터페이스에서 제공하는 preparestatement 메서드에 sql문을 전달해서 preparestatement 객체 생성
+			psmt.setString(1, id); //insert문의 각 '?'에 순서대로 회원 정보 세팅한다
 			psmt.setString(2, pwd);
 			psmt.setString(3, name);
 			psmt.setString(4, email);
-			psmt.executeUpdate();
-			psmt.close();
+			psmt.executeUpdate(); //executeUpdate 메서드는 데이터베이스에서 데이터를 추가(Insert), 삭제(Delete), 수정(Update)하는 SQL 문을 실행
+
+			psmt.close(); //jdbc 사용후 리소스 절약을 위해 닫기
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -94,12 +107,12 @@ public class MemberDAO {
 
 	public void delMember(String id) {
 		try {
-			conn=dataFactory.getConnection();
-			String query="delete from t_member" + " where id=?";
+			conn=dataFactory.getConnection(); //Datasource를 이용해 데이터베이스와 연결한다
+			String query="delete from t_member" + " where id=?"; // delete문을 문자열로 만든다, id값만 적용되게 조건(where)
 			System.out.println("prepareStatement:"+query);
-			psmt=conn.prepareStatement(query);
-			psmt.setString(1, id);
-			psmt.executeUpdate();
+			psmt=conn.prepareStatement(query); //connection인터페이스에서 제공하는 preparestatement 메서드에 sql문을 전달해서 preparestatement 객체 생성
+			psmt.setString(1, id); // 첫 번째 ?에 전달된 id를 인자로 넣는다
+			psmt.executeUpdate();//executeUpdate 메서드는 데이터베이스에서 데이터를 추가(Insert), 삭제(Delete), 수정(Update)하는 SQL 문을 실행
 			psmt.close();
 			
 		} catch (SQLException e) {
--- src/srcs/MemberServlet.java
+++ src/srcs/MemberServlet.java
@@ -11,10 +11,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/member3")
+@WebServlet("/member3") //서블릿 매핑 이름
 public class MemberServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	//요청과 관련된 api:HttpServletRequest 클래스
+	//웹 브라우저에서 전송한 정보를 톰캣 컨테이너가 HttpServletRequest 객체를 생성 후 doget,dopost,전달할 메서드 등으로 넘겨줌
+	
+	//응답과 관련된 api:HttpServletResponse 클래스
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doHandle(request,response);
 	}
@@ -25,38 +30,39 @@ public class MemberServlet extends HttpServlet {
 	}
 
 	private void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("utf-8");
-		response.setContentType("text/html;charset=utf-8");
-		MemberDAO dao=new MemberDAO();
-		PrintWriter out=response.getWriter();
-		String command=request.getParameter("command");
+		request.setCharacterEncoding("utf-8"); //전송된 데이터를 utf-8로 인코딩(한글 사용시), 즉 인코딩 설정
+		response.setContentType("text/html;charset=utf-8"); //setContentType()을 이용해 응답할 데이터 종류가 html임을 설정
 		
-		if(command!=null&&command.equals("addMember")) {
-			String _id=request.getParameter("id");
+		MemberDAO dao=new MemberDAO(); //sql문을 사용할 memberDAO 객체 생성
+		PrintWriter out=response.getWriter(); //HttpServletResponse 객체의 getWriter 메서드를 이용해서 출력 스트림인 printWriter객체를 받아옴
+		String command=request.getParameter("command"); //수행할 요청을 받아오는 것
+		
+		if(command!=null&&command.equals("addMember")) { //회원 가팁창에서 전송된 name값인 command가 addMember이면 전송된 값들을 받아온다
+			String _id=request.getParameter("id"); //request 클래스의 getParameter()를 이용해 <input>태그의 name속성 값으로 전송된 value를 가져옴
 			String _pwd=request.getParameter("pwd");
 			String _name=request.getParameter("name");
 			String _email=request.getParameter("email");
 			
-			MemberVO vo=new MemberVO();
+			MemberVO vo=new MemberVO(); //회원 가입창에서 전송된 값들을 가져와서 vo에 저장한 후 sql문을 이용해서 전달한다
 			vo.setId(_id);
 			vo.setPwd(_pwd);
 			vo.setName(_name);
 			vo.setEmail(_email);
 			
-			dao.addMember(vo);
+			dao.addMember(vo); //vo객체를 다시 arrayList에 저장한다
 		
-		}else if(command!=null&&command.equals("delMember"))
+		}else if(command!=null&&command.equals("delMember")) //command값이 delMember이면, 
 		{
-			String id=request.getParameter("id");
-			dao.delMember(id);
+			String id=request.getParameter("id"); // id를 가져와서 sql문으로 전달하여 삭제한다
+			dao.delMember(id);//delMember 메서드에 id값을 전달해서삭제
 		}
-		List<MemberVO> list=dao.listMembers();
+		List<MemberVO> list=dao.listMembers(); //listMembers() 메서드로 회원 정보를 조회한다
 		out.print("<html><body>");
 		out.print("<table border=1><tr align='center' bgcolor='blue'>");
 		out.print("<td>아이디</td><td>비밀번호</td><td>이름</td><td>이메일</td><td>가입일</td><td>삭제</td></tr>");
 		
-		for(int i=0; i<list.size(); i++) {
-			MemberVO memberVO=(MemberVO)list.get(i);
+		for(int i=0; i<list.size(); i++) { 
+			MemberVO memberVO=(MemberVO)list.get(i); //조회한 회원 정보를 for문과 <tr> 태그를 이용해 리스트로 출력한다
 			String id=memberVO.getId();
 			String pwd=memberVO.getPwd();
 			String name=memberVO.getName();
@@ -65,6 +71,7 @@ public class MemberServlet extends HttpServlet {
 			
 			out.print("<tr><td>"+id+"</td><td>"+pwd+"</td><td>"+name+"</td><td>"+email+"</td><td>"
 					+joinDate+"</td><td>"+"<a href='/ServletBoard/member3?command=delMember&id="+id+"'>삭제</a></td></tr>");
+															//삭제 클릭시 command값과 id를 서블릿으로 전송한다
 		}
 		out.print("</table></body></html>");
 		out.print("<a href='/ServletBoard/memberForm.html'>새 회원 등록하기</a>");
